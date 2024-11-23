8d49baf375141bd7ec5090b529807237d7091b53
Mauri <mauri.monzon91@gmail.com>
2024-11-23 00:07:33 -0300

    Inicio del proyecto

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { ThemedText } from "@/components/ThemedText";
+
+const PushApp = () => {
+  return (
+    <View>
+      <ThemedText>Hola mundo</ThemedText>
+    </View>
+  );
+};
+
+export default PushApp;
